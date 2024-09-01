c8d9cb94178e6314578c1198bad14533214e7ceb
ARPCharlie <charlie@arp-lan.com>
2024-09-01 22:06:27 +0200

    Fix: join message
    send embed message on join

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,22 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
     name: 'guildMemberAdd',
     execute(member, bot) {
         // Log the newly joined member to console
         console.log('User ' + member.user.tag + ' has joined the server!');
 
-        // Find a channel named welcome and send a Welcome message
-        member.guild.channels.cache.find(c => c.name === "welcome").send('Welcome '+ member.user.username)
+        // Create an embed
+        const embed = new EmbedBuilder()
+        .setColor('#0099ff')
+        .setTitle('Velkommen til serveren!')
+        .setDescription(`Velkommen ${member.user.username}`)
+        .setThumbnail(member.user.displayAvatarURL())
+        .setTimestamp()
+        .setFooter({ text: "Se info hos:" + member.guild.channels.cache.find(c => c.id === "1278084736602079284").name });
+    
+
+        // Find a channel named welcome and send the embed
+        member.guild.channels.cache.find(c => c.id === "1278084764913373184").send({ embeds: [embed] });
     }
-}
+};
