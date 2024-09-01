c0715197541f760236cc8a04660262c172d85a45
ARPCharlie <charlie@arp-lan.com>
2024-09-01 22:05:52 +0200

    Fix: update status
    update discord status to event

--- events/ready.js
+++ events/ready.js
@@ -3,9 +3,9 @@ module.exports = {
     once: true,
     execute(bot) {
         // Log Bot's username and the amount of servers its in to console
-        console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} servers!`);
+        console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} server!`);
 
         // Set the Presence of the bot user to show 'Playing: My code'
-        bot.user.setPresence({ activities: [{ name: 'My code'}] });
+        bot.user.setPresence({ activities: [{ name: 'ARP 2024 AUTUMN'}] });
     }
 }
