e3288289fe68e023cf31b0dd514f9c8ab7e64967
ARPCharlie <charlie@arp-lan.com>
2024-09-03 11:32:44 +0200

    Chore: collections
    add collections

--- dbclient.js
+++ dbclient.js
@@ -1,21 +1,31 @@
-  const { MongoClient } = require('mongodb');
-  const config = require('./config.json');
+const { MongoClient } = require('mongodb');
+const config = require('./config.json');
 
-  const uri = config.uri;
-  const dbName = 'BOT';
+const uri = config.uri;
+const dbName = 'BOT';
 
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  async function connectToDatabase() {
-    try {
-      await client.connect();
-      console.log('Connected successfully to MongoDB');
-      const db = client.db(dbName);
-      return db;
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
-      throw error;
-    }
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected successfully to MongoDB');
+    
+    const db = client.db(dbName);
+
+    // Add the collections you want to use
+    const collections = {
+      users: db.collection('users'),
+      permissions: db.collection("users"),
+      blogs: db.collection("blogs"),
+      // Add more collections as needed
+    };
+
+    return collections;
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
   }
+}
 
-  module.exports = { connectToDatabase };
+module.exports = { connectToDatabase };
