33a0ae46bf799f82a7a0d2f03b000f1b0fcaccc2
ARPCharlie <charlie@arp-lan.com>
2024-09-03 20:59:16 +0200

    Chore: cleanup
    remove useless code

--- dbclient.js
+++ dbclient.js
@@ -1,27 +1,13 @@
 const { MongoClient } = require('mongodb');
-const config = require('./config.json');
+const { uri, dbName } = require("./config.json");
 
-const uri = config.uri;
-const dbName = 'BOT';
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri);
 
 async function connectToDatabase() {
   try {
     await client.connect();
     console.log('Connected successfully to MongoDB');
-    
-    const db = client.db(dbName);
-
-    // Add the collections you want to use
-    const collections = {
-      users: db.collection('users'),
-      permissions: db.collection("users"),
-      blogs: db.collection("blogs"),
-      // Add more collections as needed
-    };
-
-    return collections;
+    return client.db(dbName); // Make sure to return the specific database
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     throw error;
